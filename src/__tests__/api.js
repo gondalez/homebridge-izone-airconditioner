85fb1de4228d53ce58fb45fec7fa0683a0b06104
tris <gondalez@gmail.com>
2020-02-01 08:48:44 +0800

    Extract test helper for read tests

--- src/__tests__/api.js
+++ src/__tests__/api.js
@@ -2,7 +2,68 @@ import api from '../api'
 import { MODES } from '../api'
 import systemSettingsResponse from '../__fixtures__/systemSettings'
 
-const testWriteAction = (doAction, expectedPath, expectedBody) => {
+test('setPower on', () => {
+  testWrite(client => client.setPower(true), 'SystemON', '{"SystemON":"on"}')
+})
+
+test('setPower off', () => {
+  testWrite(client => client.setPower(false), 'SystemON', '{"SystemON":"off"}')
+})
+
+test('setMode cool', () => {
+  testWrite(
+    client => client.setMode(MODES.cool),
+    'SystemMODE',
+    '{"SystemMODE":"cool"}'
+  )
+})
+
+test('setUnitSetpoint', () => {
+  testWrite(
+    client => client.setUnitSetpoint(24.0),
+    'UnitSetpoint',
+    '{"UnitSetpoint":"24"}'
+  )
+})
+
+test('getUnitSetpoint', () => {
+  testRead(client => client.getUnitSetpoint(), 25.0)
+})
+
+test('getActualTemperature', () => {
+  testRead(client => client.getActualTemperature(), 29.0)
+})
+
+test('getMode', () => {
+  testRead(client => client.getMode(), MODES.cool)
+})
+
+test('error handling', () => {
+  const fetch = jest.fn()
+  fetch.mockResolvedValueOnce({ ok: false, statusText: 'TEST RESULT' })
+  fetch.mockResolvedValueOnce({})
+
+  const client = api('http://example.com/', fetch)
+
+  client
+    .setPower(true)
+    .catch(e => expect(e.toString()).toEqual('API ERROR: TEST RESULT'))
+})
+
+const testRead = (action, expectedResult) => {
+  const fetch = jest.fn()
+  fetch.mockResolvedValueOnce({ ok: true, json: () => systemSettingsResponse })
+
+  const client = api('http://example.com/', fetch)
+
+  action(client).then(result => {
+    expect(result).toEqual(expectedResult)
+  })
+
+  expect(fetch.mock.calls).toEqual([['http://example.com/SystemSettings']])
+}
+
+const testWrite = (doAction, expectedPath, expectedBody) => {
   const fetch = jest.fn()
   fetch.mockResolvedValueOnce({ ok: true })
   fetch.mockResolvedValueOnce({})
@@ -22,86 +83,3 @@ const testWriteAction = (doAction, expectedPath, expectedBody) => {
     ],
   ])
 }
-
-test('setPower on', () => {
-  testWriteAction(
-    client => client.setPower(true),
-    'SystemON',
-    '{"SystemON":"on"}'
-  )
-})
-
-test('setPower off', () => {
-  testWriteAction(
-    client => client.setPower(false),
-    'SystemON',
-    '{"SystemON":"off"}'
-  )
-})
-
-test('setPower on error', () => {
-  const fetch = jest.fn()
-  fetch.mockResolvedValueOnce({ ok: false, statusText: 'TEST RESULT' })
-  fetch.mockResolvedValueOnce({})
-
-  const client = api('http://example.com/', fetch)
-
-  client
-    .setPower(true)
-    .catch(e => expect(e.toString()).toEqual('API ERROR: TEST RESULT'))
-})
-
-test('setMode cool', () => {
-  testWriteAction(
-    client => client.setMode(MODES.cool),
-    'SystemMODE',
-    '{"SystemMODE":"cool"}'
-  )
-})
-
-test('setUnitSetpoint', () => {
-  testWriteAction(
-    client => client.setUnitSetpoint(24.0),
-    'UnitSetpoint',
-    '{"UnitSetpoint":"24"}'
-  )
-})
-
-test('getUnitSetpoint', () => {
-  const fetch = jest.fn()
-  fetch.mockResolvedValueOnce({ ok: true, json: () => systemSettingsResponse })
-
-  const client = api('http://example.com/', fetch)
-
-  client.getUnitSetpoint().then(result => {
-    expect(result).toEqual(25.0)
-  })
-
-  expect(fetch.mock.calls).toEqual([['http://example.com/SystemSettings']])
-})
-
-test('getActualTemperature', () => {
-  const fetch = jest.fn()
-  fetch.mockResolvedValueOnce({ ok: true, json: () => systemSettingsResponse })
-
-  const client = api('http://example.com/', fetch)
-
-  client.getActualTemperature().then(result => {
-    expect(result).toEqual(29.0)
-  })
-
-  expect(fetch.mock.calls).toEqual([['http://example.com/SystemSettings']])
-})
-
-test('getMode', () => {
-  const fetch = jest.fn()
-  fetch.mockResolvedValueOnce({ ok: true, json: () => systemSettingsResponse })
-
-  const client = api('http://example.com/', fetch)
-
-  client.getMode().then(result => {
-    expect(result).toEqual(MODES.cool)
-  })
-
-  expect(fetch.mock.calls).toEqual([['http://example.com/SystemSettings']])
-})
