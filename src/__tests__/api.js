1a2f9fb9b73d79236d6de8858c0279ba43b58c02
tris <gondalez@gmail.com>
2020-02-01 15:05:05 +0800

    Fix tests around api promises

--- src/__tests__/api.js
+++ src/__tests__/api.js
@@ -2,57 +2,44 @@ import api from '../api'
 import { MODES } from '../api'
 import systemSettingsResponse from '../__fixtures__/systemSettings'
 
-test('getPower', () => {
-  testRead(client => client.getPower(), false)
-})
+test('getPower', () => testRead(client => client.getPower(), false))
 
-test('setPower on', () => {
-  testWrite(client => client.setPower(true), 'SystemON', '{"SystemON":"on"}')
-})
+test('setPower on', () =>
+  testWrite(client => client.setPower(true), 'SystemON', '{"SystemON":"on"}'))
 
-test('setPower off', () => {
-  testWrite(client => client.setPower(false), 'SystemON', '{"SystemON":"off"}')
-})
+test('setPower off', () =>
+  testWrite(client => client.setPower(false), 'SystemON', '{"SystemON":"off"}'))
 
-test('setMode cool', () => {
+test('setMode cool', () =>
   testWrite(
     client => client.setMode(MODES.cool),
     'SystemMODE',
     '{"SystemMODE":"cool"}'
-  )
-})
+  ))
 
-test('setUnitSetpoint', () => {
+test('setUnitSetpoint', () =>
   testWrite(
     client => client.setUnitSetpoint(24.0),
     'UnitSetpoint',
     '{"UnitSetpoint":"24"}'
-  )
-})
+  ))
 
-test('getUnitSetpoint', () => {
-  testRead(client => client.getUnitSetpoint(), 25.0)
-})
+test('getUnitSetpoint', () =>
+  testRead(client => client.getUnitSetpoint(), 25.0))
 
-test('getActualTemperature', () => {
-  testRead(client => client.getActualTemperature(), 29.0)
-})
+test('getActualTemperature', () =>
+  testRead(client => client.getActualTemperature(), 29.0))
 
-test('getMode', () => {
-  testRead(client => client.getMode(), MODES.cool)
-})
+test('getMode', () => testRead(client => client.getMode(), MODES.cool))
 
-test('getFanSpeed', () => {
-  testRead(client => client.getFanSpeed(), 'medium')
-})
+test('getFanSpeed', () => testRead(client => client.getFanSpeed(), 'med'))
 
-test('setFanSpeed med', () => {
+test('setFanSpeed med', () =>
   testWrite(
     client => client.setFanSpeed('med'),
     'SystemFAN',
     '{"SystemFAN":"med"}'
-  )
-})
+  ))
 
 test('error handling', () => {
   const fetch = jest.fn()
@@ -61,20 +48,17 @@ test('error handling', () => {
 
   const client = api('http://example.com/', fetch)
 
-  client
+  return client
     .setPower(true)
     .catch(e => expect(e.toString()).toEqual('API ERROR: TEST RESULT'))
 })
 
-test('url with no trailing slash', () => {
-  testUrl('http://example.com')
-})
+test('url with no trailing slash', () => testUrl('http://example.com'))
 
-test('invalid url', () => {
+test('invalid url', () =>
   expect(() => {
     api('example.com')
-  }).toThrow('Not a valid URL')
-})
+  }).toThrow('Not a valid URL'))
 
 const testRead = (action, expectedResult) => {
   const fetch = jest.fn()
@@ -82,11 +66,13 @@ const testRead = (action, expectedResult) => {
 
   const client = api('http://example.com/', fetch)
 
-  action(client).then(result => {
+  const promise = action(client).then(result => {
     expect(result).toEqual(expectedResult)
   })
 
   expect(fetch.mock.calls).toEqual([['http://example.com/SystemSettings']])
+
+  return promise
 }
 
 const testWrite = (doAction, expectedPath, expectedBody) => {
@@ -96,7 +82,7 @@ const testWrite = (doAction, expectedPath, expectedBody) => {
 
   const client = api('http://example.com/', fetch)
 
-  doAction(client)
+  const promise = doAction(client)
 
   expect(fetch.mock.calls).toEqual([
     [
@@ -108,6 +94,8 @@ const testWrite = (doAction, expectedPath, expectedBody) => {
       },
     ],
   ])
+
+  return promise
 }
 
 const testUrl = url => {
@@ -116,7 +104,9 @@ const testUrl = url => {
 
   const client = api(url, fetch)
 
-  client.getPower().then(result => {})
+  const promise = client.getPower().then(result => {})
 
   expect(fetch.mock.calls).toEqual([['http://example.com/SystemSettings']])
+
+  return promise
 }
